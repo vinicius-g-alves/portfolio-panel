75d61c0b7c890cd892f0db4911b8a71acace886a
vinicius-g-alves <vinicius.garcia.alves10@gmail.com>
2025-03-05 00:09:27 -0300

    add antd on project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import RoutesPage from "./Routes";
 
 function App() {
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,45 @@
+import { Breadcrumb, Layout, Menu } from "antd";
+
 const HomePage = () => {
   return (
-    <>
-      <a href="/project">Vá para o projeto!</a>
-    </>
+    <Layout>
+      <Layout.Header style={{ display: "flex", alignItems: "center" }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={[]}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Layout.Header>
+      <Layout>
+        <Layout.Sider width={200}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%", borderRight: 0 }}
+            items={[]}
+          />
+        </Layout.Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Breadcrumb
+            items={[{ title: "Home" }, { title: "List" }, { title: "App" }]}
+            style={{ margin: "16px 0" }}
+          />
+          <Layout.Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Layout.Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 };
 
